84b388b6ce6095d21a3d4a1f8d5a8c815815b529
hollowdll <pixerchat@gmail.com>
2022-10-24 12:28:22 +0300

    init week1 exercises

---
+++ src/week1/exercises/HenkiloMapSovellus.java
@@ -0,0 +1,42 @@
+package week1.exercises;
+
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+
+class HenkiloMapSovellus {
+	
+	private Scanner input = new Scanner(System.in);
+	private Map<String, Henkilo> henkilot = new HashMap<String, Henkilo>();
+	
+	void kysyValinta() {
+		
+	}
+	
+	void lisaaHenkilo() {
+		
+	}
+	
+	void naytaHenkilonTiedot() {
+		
+	}
+	
+	void muutaHenkilonNimiJaOsoite() {
+		
+	}
+	
+	void muutaHenkilonKoko() {
+		
+	}
+	
+	void naytaKaikkiHenkilot() {
+		
+	}
+
+	public static void main(String[] args) {
+		HenkiloMapSovellus ohjelma = new HenkiloMapSovellus();
+		
+		
+	}
+
+}
